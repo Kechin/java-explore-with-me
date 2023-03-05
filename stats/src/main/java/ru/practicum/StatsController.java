222af206e964d6d03566e93de498287663f4d528
seer <kechin13@gmail.com>
2023-03-05 23:56:25 +0000

    Добавлен основной сервис.

--- stats/src/main/java/ru/practicum/StatsController.java
+++ stats/src/main/java/ru/practicum/StatsController.java
@@ -4,6 +4,7 @@ package ru.practicum;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -23,6 +24,7 @@ public class StatsController {
     }
 
     @PostMapping("/hit")
+    @ResponseStatus(code = HttpStatus.CREATED)
     void create(@RequestBody HitDto hitDto) {
         statsService.addHit(hitDto);
     }

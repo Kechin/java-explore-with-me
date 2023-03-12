ee49bc17f5b32809b5f61812b090979259f4b61d
seer <kechin13@gmail.com>
2023-03-12 18:51:46 +0000

    Работа над ошибками.

--- stats/stats-client/src/main/java/ru/practicum/client/HitSender.java
+++ stats/stats-client/src/main/java/ru/practicum/client/HitSender.java
@@ -3,8 +3,6 @@ package ru.practicum.client;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.DefaultUriBuilderFactory;
@@ -22,13 +20,15 @@ import java.util.Set;
 public class HitSender extends BaseClient {
     private static final String apiPrefix = "/hit";
     private static final String getPrefix = "/stats?";
+    private String url;
 
-    @Autowired
-    public HitSender(@Value("${stat-server.url}") String serverUrl, RestTemplateBuilder builder) {
+
+    public HitSender(String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                         .build()
         );
+        url = serverUrl;
     }
 
     public ResponseEntity<Object> createHit(HttpServletRequest httpServletRequest) {
@@ -44,7 +44,7 @@ public class HitSender extends BaseClient {
         }
         RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
-        var response = request.get("http://localhost:9090" + getPrefix + requestUris);
+        var response = request.get(url + getPrefix + requestUris);
         if (response == null || response.getBody().toString().isEmpty()) {
             return new ArrayList<>();
         }
